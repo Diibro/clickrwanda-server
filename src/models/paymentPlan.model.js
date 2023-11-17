c7352bc3989673d5de72729b3ac93a905ee308d0
Dushime Brother <dushimeconso1234@gmail.com>
2023-11-17 14:29:55 +0200

    update the payment plan model

--- src/models/paymentPlan.model.js
+++ src/models/paymentPlan.model.js
@@ -1,13 +1,98 @@
 const db = require('../configs/database.config');
-const {v4: uuidv4} = require('uuid');
+const dbErrorHandler = require('../middlewares/dbError');
 
 const payPlanModel = {
-     queries: {},
-     add: async(req, res) => {},
-     update: async(req, res) => {},
-     findAll: async(req, res) => {},
-     delete: async(req, res) => {},
-     search: async(req, res) => {},
+     name:"payment plan",
+     queries: {
+          getAll: "select * from payment_plan",
+          addPaymentPlan : 'insert into payment_plan values(?,?, ?, ?)',
+          deleteById: 'delete from payment_plan where plan_id=?',
+          updateById: 'update payment_plan set plan_name = ?, plan_amount = ?, description = ? where plan_id = ?',
+          searchByName: 'select * from payment_plan where plan_name = ?',
+          searchById: 'select * from payment_plan where plan_id =?'
+     },
+     add: async(req, res) => {
+          try {
+               const info = req.body;
+               let description = JSON.stringify(info.description) || null;
+               console.log(info.plan_amount);
+               const price = Number(info.plan_amount);
+               const values = [info.plan_id, info.plan_name,price, description]
+               db.query(payPlanModel.queries.addPaymentPlan, values, (err) => {
+                    if(err){
+                         return dbErrorHandler(err,res, payPlanModel.name);
+                    }
+                    return res.json({status:'pass', message:"sucessfully added the payment plan"});
+               });
+          } catch (error) {
+               return res.json({status: 'fail', message: 'server error', error});
+          }
+     },
+     update: async(req, res) => {
+          try {
+               const info = req.body;
+               db.query(payPlanModel.queries.searchById, [info.plan_id], (error, data) =>{
+                    if(error){
+                         return dbErrorHandler(err, res, payPlanModel.name);
+                    }
+                    if(!data[0]){
+                         return res.json({status: 'fail', message:"payment plan does not exist in our database"});
+                    }
+                    const values = [info.plan_name || data[0].plan_name, Number(info.plan_amount) || data[0].plan_amount, JSON.stringify(info.description || data[0].description), info.plan_id];
+                    db.query(payPlanModel.queries.updateById, values, (err) => {
+                         if(err){
+                              return dbErrorHandler(err, res, payPlanModel.name);
+                         }
+                         return res.json({status: 'pass', message: 'successfully updated'});
+                    });
+               });
+               
+          } catch (error) {
+               return res.json({status:'failed', message: 'server', error});
+          }
+     },
+     findAll: async(req, res) => {
+          try {
+               db.query(payPlanModel.queries.getAll, (err, data) => {
+                    if(err){
+                         return dbErrorHandler(err, res, payPlanModel.name);
+                    }
+                    return res.json({status: 'pass', data});
+               });
+          } catch (error) {
+               return res.json({status:"fail", message: "server error", error});
+          }
+     },
+     delete: async(req, res) => {
+          try {
+               const info = req.body;
+               db.query(payPlanModel.queries.deleteById, [info.plan_id], (err) => {
+                    if(err){
+                         return dbErrorHandler(err, res, payPlanModel.name);
+                    }
+                    return res.json({status: 'pass', message: 'deleted the payment plan successfully'});
+               });
+          } catch (error) {
+               return res.json({status: 'fail', message: 'server error', error});
+          }
+     },
+     search: async(req, res) => {
+          try {
+               const info = req.body;
+               db.query(payPlanModel.queries.searchById, [info.plan_id], (error, data) =>{
+                    if(error){
+                         return dbErrorHandler(err, res, payPlanModel.name);
+                    }
+                    if(!data[0]){
+                         return res.json({status: 'fail', message:"payment plan does not exist in our database"});
+                    }
+                    return res.json({status: 'pass', data});
+               });
+               
+          } catch (error) {
+               return res.json({status:'failed', message: 'server', error});
+          }
+     },
 };
 
 module.exports = payPlanModel;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ const subCatRouter = require('./sub_category');
 const userRouter = require('./user');
 const advertRouter = require('./advert');
 const payMethodRouter = require('./payment_method');
+const paymentPlanRouter = require('./payment_plan');
 
 mainRouter.use('/users', userRouter);
 mainRouter.use('/category', categoryRouter);
 mainRouter.use('/advert', advertRouter);
 mainRouter.use('payment-methods', payMethodRouter);
 mainRouter.use('/sub-category', subCatRouter);
+mainRouter.use('/payment-plan', paymentPlanRouter);
 
 
 module.exports = mainRouter;
