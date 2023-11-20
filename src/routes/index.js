44caf67eb8087b2549a50b51c0157b592e4ceacb
Dushime Brother <dushimeconso1234@gmail.com>
2023-11-20 15:32:28 +0200

    working on the test route

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,9 @@ mainRouter.use('/advert', advertRouter);
 mainRouter.use('payment-methods', payMethodRouter);
 mainRouter.use('/sub-category', subCatRouter);
 mainRouter.use('/payment-plan', paymentPlanRouter);
+mainRouter.get('/', (req, res) => {
+     res.json({Status: "pass", message: "Server is up and running"});
+});
 
 
 module.exports = mainRouter;
