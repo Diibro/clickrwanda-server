bd9f22074165111efb9f5b07c4194d9bd2ba6404
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-03 06:55:35 +0200

    cors configuration

--- src/middlewares/middleWare.js
+++ src/middlewares/middleWare.js
@@ -8,17 +8,8 @@ const middleWares = (app) => {
      app.use(compression());
      app.use(express.urlencoded({extended:true}));
      app.use(express.json());
-     // app.use(corsMiddleWare());
+     app.use(corsMiddleWare());
      app.use('/public',express.static('./public'));
-     app.use((req, res, next) => {
-          //allow access to current url. work for https as well
-          res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
-          res.removeHeader('x-powered-by');
-          //allow access to current method
-          res.setHeader('Access-Control-Allow-Methods',req.method);
-          res.setHeader('Access-Control-Allow-Headers','Content-Type');
-          next();
-        });
 }
 
 
