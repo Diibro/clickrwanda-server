67804fe08ade786d6aec7d44b148c1dc85ff1f1e
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-04 09:39:18 +0200

    cors config

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -7,19 +7,17 @@ const corsMiddleWare = () => {
     'http://localhost:3002',
     'http://localhost:5173',
     'https://clickrwanda.com',
+    'https://www.clickrwanda.com',
     'https://clickrwanda-client.vercel.app/'
   ];
 
-  if(process.env.NODE_ENV === 'development'){
-    return cors({
-      origin: acceptedUrls,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    });
-  }
+  return cors({
+    origin: acceptedUrls,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
-  return cors();
 
   
   
