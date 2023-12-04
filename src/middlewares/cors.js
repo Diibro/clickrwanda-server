04e7ebbb20c8fb68df80e3e6d258e5b5085441cc
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-04 09:25:25 +0200

    cors again

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -5,11 +5,8 @@ const corsMiddleWare = () => {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
-    'https://clickrwanda.com',
     'http://localhost:5173',
     'https://clickrwanda.com',
-    'https://www.clickrwanda.com',
-    'http://localhost:5173/',
     'https://clickrwanda-client.vercel.app/'
   ];
 
