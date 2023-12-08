f540abcbe5dd57411c6f994825bcf0c70d81c8c2
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-08 07:22:22 +0200

    added none samesite

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -188,7 +188,7 @@ const userModel = {
           res.cookie('token', token, {
                httpOnly: true,
                secure: process.env.NODE_ENV === 'production' ? true : false,
-               // sameSite: 'None',
+               sameSite: 'None',
                expiresIn: 2 * 60 * 60, 
           });
      
