0f3f5cc75532bcdb1032c8619f07144b7035e35b
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-12 16:56:37 +0200

    updated auth function

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const authenticateUser = (req, res, next) => {
      const token = req.body.loginToken || null;
      if (!token){
-          return res.status(401).json({ status: "fail", message: 'No token provided.' });
+          return res.json({ status: "fail", message: 'No token provided.' });
      }
 
      jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
           if(err){
-               return res.status(401).json({ status: "fail", message: 'invalid token' });
+               return res.json({ status: "fail", message: 'invalid token' });
           }
           req.userId = decoded.userId;
           next();
