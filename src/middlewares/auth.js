c3dc3cd9b3253ca4cfb1b07a36b3d8ccfe4731a7
Dushime Brother <dushimeconso1234@gmail.com>
2023-12-12 16:52:22 +0200

    update authentication middle ware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const authenticateUser = (req, res, next) => {
      const token = req.body.loginToken || null;
      if (!token){
-          return res.status(401).json({ auth: false, message: 'No token provided.' });
+          return res.status(401).json({ status: "fail", message: 'No token provided.' });
      }
 
      jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
           if(err){
-               return res.status(401).json({ auth: false, message: 'invalid token' });
+               return res.status(401).json({ status: "fail", message: 'invalid token' });
           }
           req.userId = decoded.userId;
           next();
