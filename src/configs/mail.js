063a4d9529e66b5f967717c2bd689777cdc5827b
Dushime Brother <dushimeconso1234@gmail.com>
2024-01-06 22:22:18 +0200

    updated the mail setup

--- src/configs/mail.js
+++ src/configs/mail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
      host: 'smtp.office365.com',
      port: 587,
-     secure: false,
+     secure: true,
      auth: {
      user: process.env.EMAIL_USER,
      pass: process.env.EMAIL_PASS,
