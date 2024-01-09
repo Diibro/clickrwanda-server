0edda9aba06cbd4cc95f657632c5c59abb961376
Dushime Brother <dushimeconso1234@gmail.com>
2024-01-09 21:51:46 +0200

    updated the boosted ads functionality

--- src/models/advert.model.js
+++ src/models/advert.model.js
@@ -20,7 +20,8 @@ const advertModel = {
           countAll: 'select count(*) as totalAdverts from adverts',
           findAll: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating, category.category_name, category.category_id from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id order by a.ad_date desc limit 50 ;",
           getCategory: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating, category.category_id,category.category_name from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where category.category_id = ?;",
-          findAllPaged: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating, category.category_name, category.category_id from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id order by a.ad_date desc limit 50 offset ?;",
+          findAllPaged: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating, category.category_name, category.category_id from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where p.plan_name like '%freemium%' order by a.ad_date desc limit 50 offset ?;",
+          findBoosted: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating, category.category_name, category.category_id from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where p.plan_name like '%premium%' or p.plan_name like '%urgent%' or p.plan_name like '%featured%' order by a.ad_date  desc limit ?;",
           // searchAdverts: "select (char_length(a.ad_name) - char_length(replace(a.ad_name, ?, ''))) / char_length(?) * 100 as inclusion_percentage, a.ad_id, a.ad_name, a.ad_image, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_name, p.plan_name, u.full_name, u.user_location,u.user_phone, u.user_email, category.category_id,category.category_name from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where a.ad_name like concat('%',?,'%') order by inclusion_percentage desc;",
           searchAdverts: "select (char_length(a.ad_name) - char_length(replace(a.ad_name, ?, ''))) / char_length(?) * 100 as inclusion_percentage, a.ad_id, a.ad_name, a.ad_image, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_name, p.plan_name, u.full_name, u.user_location,u.user_phone, u.user_email, category.category_id,category.category_name from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where a.ad_name like concat('%',?,'%') order by inclusion_percentage desc;",
           searchAdvertsSub: "select a.ad_id, a.ad_name, a.description, a.ad_image, a.ad_images, a.ad_type, a.ad_price, a.ad_date, a.status, a.contact, a.ad_views, c.sub_id, c.sub_name, p.plan_name, u.full_name, u.user_location, u.profile_image,u.user_phone, u.user_email, u.rating,category.category_id, category.category_name from adverts a inner join users u on a.ad_user_id = u.user_id inner join sub_category c on a.sub_category_id = c.sub_id inner join payment_plan p on a.ad_plan_id = p.plan_id inner join category  on c.parent_id = category.category_id where c.sub_id in (select sub_id from sub_category where sub_name like concat('%', ?, '%'));",
@@ -114,8 +115,18 @@ const advertModel = {
           try{
                const info  = req.body;
                let adsFetched = [];
+               let boostedAds = [];
                let totalAds = 0;
                await Promise.all([
+                    info.boost ?
+                    new Promise(resolve => {
+                         db.query(advertModel.queries.findBoosted, [info.boost], (err, result) => {
+                              if(err) boostedAds = "error databse";
+                              else boostedAds = result;
+                              resolve();
+                         } )
+                    }) : null ,
+                    info.page ?
                     new Promise(resolve => {
                          db.query(advertModel.queries.findAllPaged,[info.page], (err, data) => {
                               if(err){
@@ -124,16 +135,17 @@ const advertModel = {
                               adsFetched = data;
                               resolve();
                          })
-                    }),
+                    }) : null,
+                    info.page ?
                     new Promise((resolve)=>db.query(advertModel.queries.countAll, (err, data) => {
                          if(err) totalAds = 0;
                          else totalAds = parseInt(data[0].totalAdverts);
                          resolve();
-                    }) )
+                    }) ) : null
                ]); 
-               return res.json({status:"pass", message:"success",data: adsFetched[0] ? adsFetched : "no data found", totalAds});
+               return res.json({status:"pass", message:"success",data: {generalAds:adsFetched[0] ? adsFetched : "no data found", boostedAds}, totalAds});
           }catch(error){
-               return res.json({status:"fail", message:"server error", error})
+               return res.json({status:"fail", message:"server error", error});
           }
      },
      delete: async(req, res) => {
