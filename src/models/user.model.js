d5b7be7187051ec634d92920ddbd09a853595ec6
Dushime Brother <dushimeconso1234@gmail.com>
2024-01-11 22:09:20 +0200

    updates

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,9 +23,9 @@ const userModel = {
           searchByid: "select * from users where user_id = ? ",
           updateUserRating: "update users set rating = ? where user_id = ?",
           getUserViews: "select sum(a.ad_views) as total_views from adverts a inner join users u on a.ad_user_id = u.user_id where u.user_id = ?;",
-          getBestViewedUsers: "select u.user_id, u.username, u.full_name, u.profile_image, u.user_phone, sum(a.ad_views) as total_ads from users u inner join adverts a on u.user_id = a.ad_user_id group by u.user_id having total_ads > 50 order by total_ads desc;",
+          getBestViewedUsers: "select u.user_id, u.username, u.full_name, u.profile_image, u.user_phone, sum(a.ad_views) as total_views, count(a.ad_id) as total_ads from users u inner join adverts a on u.user_id = a.ad_user_id group by u.user_id having total_views > 50 order by total_views desc;",
           getUserAdsTotal: "select count(*) as total_ads from adverts where ad_user_id = ?",
-          getBestSellers: "select u.user_id, u.username, u.full_name,u.profile_image,  u.user_phone, p.plan_name, sum(a.ad_views) as total_ads from users u inner join payment_plan p on u.ad_plan_id = p.plan_id inner join adverts a on u.user_id = a.ad_user_id group by u.user_id where p.plan_name like '%premium%' or p.plan_name like '%urgent%' or p.plan_name like '%featured%';",
+          getBestSellers: "select u.user_id, u.username, u.full_name,u.profile_image, u.user_phone, p.plan_name, sum(a.ad_views) as total_views, count(a.ad_id) as total_ads from users u inner join payment_plan p on u.ad_plan_id = p.plan_id inner join adverts a on u.user_id = a.ad_user_id group by u.user_id where p.plan_name like '%premium%' or p.plan_name like '%urgent%' or p.plan_name like '%featured%';",
           changePassword: "update users set user_password = ? where user_id = ? ;"
      },
      findAll: async(req, res) => {
