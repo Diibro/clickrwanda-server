6d0523d83d03c9fcb1efc42624995d3cecdd2c72
Dushime Brother <dushimeconso1234@gmail.com>
2024-02-20 14:15:30 +0200

    updated on db check connection

--- src/middlewares/checkDbConnection.js
+++ src/middlewares/checkDbConnection.js
@@ -1,21 +1,25 @@
 const db = require("../configs/database.config")
 
 const checkDbConnection = (req, res, next) => {
-     db.query('select 1', (error, res) => {
-          if(error){
-               db.connect((err) => {
-                    if (err) {
-                    console.error('Error connecting to the database:', err);
-                    res.status(500).send('Internal Server Error');
-                    } else {
-                    console.log('Re - Connected to the database');
-                    next(); 
-                    }
-               });
-          }else{
-               next();
-          }
-     })
+     try {
+          db.query('select 1', (error, res) => {
+               if(error){
+                    db.connect((err) => {
+                         if (err) {
+                              console.error('Error connecting to the database:', err);
+                         } else {
+                         console.log('Re - Connected to the database');
+                         next(); 
+                         }
+                    });
+               }else{
+                    next();
+               }
+          })
+     } catch (e) {
+          console.error(e)
+     }
+     
 }
 
 module.exports = checkDbConnection;
