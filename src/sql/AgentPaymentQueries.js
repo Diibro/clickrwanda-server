b24c1d6aaabb70069974eaefde365ce08a18df8a
Dushime Brother <dushimeconso1234@gmail.com>
2024-07-10 11:58:03 +0200

    modified the agent payment queries

--- src/sql/AgentPaymentQueries.js
+++ src/sql/AgentPaymentQueries.js
@@ -1,6 +1,6 @@
 
 module.exports =  {
-     selectAll: "select * from agent_payment order by p_date desc",
+     selectAll: "select p.p_date, p.amount, p.agent_id, p.status, a.a_phone, a.a_name from agent_payment p inner join agents a on p.agent_id = a.agent_id order by p_date desc",
      insertOne: "insert into agent_payment values (?,?,?,?)",
      selectByAgentId: "select * from agent_payment where agent_id = ? order by p_date desc",
      update: "update agent_payment set status = ?, amount = ? where agent_id = ? and date = ?"
