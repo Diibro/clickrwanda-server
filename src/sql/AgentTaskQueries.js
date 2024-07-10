166c8236609d962d2dfc93d3992c9e1238fe1c9c
Dushime Brother <dushimeconso1234@gmail.com>
2024-07-10 20:36:39 +0200

    Updated the queries

--- src/sql/AgentTaskQueries.js
+++ src/sql/AgentTaskQueries.js
@@ -1,7 +1,7 @@
 module.exports = {
-     selectAll: "select * from agent_task order task_date desc;",
+     selectAll: "select task_name, assigned_agents, date_format(task_date,'%Y-%m-%d') as task_date, date_format(exp_date, '%Y-%m-%d') as exp_date, v_ids,  from agent_task order task_date desc;",
      insertOne: "insert into agent_task values (?,?,?,?,?)",
      update: "update agent_task set v_ids = ?, assigned_agents = ?, exp_date = ? where task_name = ? and task_date = ?",
      delete: "delete from agent_task where task_name = ? and task_date = ?;",
-     selectByAgent: 'select t.task_date, t.exp_date, t.v_ids from agent_task t where JSON_CONTAINS(t.assigned_agents, ?);'
+     selectByAgent: "select date_format(t.task_date, '%Y-%m-%d') as task_date, date_format(t.exp_date, '%Y-%m-%d') as exp_date, t.v_ids from agent_task t where JSON_CONTAINS(t.assigned_agents, ?);"
 }
