dcf713e52d3128595512e9b5d21ef6f690df9d9d
Dushime Brother <dushimeconso1234@gmail.com>
2024-07-11 11:44:46 +0200

    updated the agent query

--- src/sql/PlanSubscription.js
+++ src/sql/PlanSubscription.js
@@ -2,6 +2,6 @@ module.exports = {
      selectAll: "select * from plan_subscription;",
      insertOne: "insert into plan_subscription values(?,?,?,?,?,?,?,?,?);",
      update: "update plan_subscription set plan_type = ?, amount = ?, status = ?, exp_date = ? where plan_id = ? and user_id = ? and subscription_date = ?",
-     selectByR_Id: "select * from plan_subscription where r_id = ? and status = 'Approved",
+     selectByR_Id: "select * from plan_subscription where r_id = ? and status = 'Approved';",
      selectByUserId: "select * from plan_subscription where user_id = ?"
 }
